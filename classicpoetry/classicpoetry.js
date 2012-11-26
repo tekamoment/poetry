25415213b89079baf0cc13c3be53a3ff345f7a98
Carlos Arcenas <carlosarcenas@yahoo.com>
2012-11-26 20:37:46 +0700

    Metrofied classicpoetry. Moved poems to js file.

---
+++ classicpoetry/classicpoetry.js
@@ -0,0 +1,14 @@
+function sonnet116() {
+	var headerHTML = "<h2>sonnet 116</h2>";
+	var poemHTML = "<p>Let me not to the marriage of true minds</p> <p>Admit impediments. Love is not love</p> <p>Which alters when it alteration finds,</p> <p>Or bends with the remover to remove:</p> <p>O no! it is an ever-fixed mark</p> <p>That looks on tempests and is never shaken;</p> <p>It is the star to every wandering bark,</p> <p>Whose worth's unknown, although his height be taken.</p> <p>Love's not Time's fool, though rosy lips and cheeks</p> <p>Within his bending sickle's compass come:</p> <p>Love alters not with his brief hours and weeks,</p> <p>But bears it out even to the edge of doom.</p> <p>If this be error and upon me proved,</p> <p>I never writ, nor no man ever loved.</p>";
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = poemHTML;
+}
+
+
+function sonnet130() {
+	var headerHTML = "<h2>sonnet 130</h2>";
+	var poemHTML = "<p>My mistress' eyes are nothing like the sun,</p> <p>Coral is far more red than her lips' red,</p> <p>If snow be white, why then her breasts are dun,</p> <p>If hair be wires, black wires grow on her head.</p> <p>I have seen roses damasked, red and white,</p> <p>But no such roses see I in her cheeks;</p> <p>And in some perfumes is there more delight</p> <p>Than in the breath that from my mistress reeks.</p> <p>I love to hear her speak, yet well I know</p> <p>That music hath a far more pleasing sound;</p> <p>I grant I never saw a goddess go;</p> <p>My mistress when she walks treads on the ground.</p> <p>And yet, by heaven, I think my love as rare</p> <p>As any she belied with false compare.</p>";
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = poemHTML;
+}
