58c5e4e5ff721f8af103f8f31bb35587b1307304
Carlos Arcenas <carlosarcenas@yahoo.com>
2012-11-26 19:44:42 +0700

    Filed in OCaptain analyses. Changed Raven to Captain.

--- originalpoetry/originalpoetry.js
+++ originalpoetry/originalpoetry.js
@@ -1,14 +1,14 @@
 
 function poetryOriginal() {
 	var headerHTML = "<h2>	poetry - original version </h2>";
-	var poemHTML = "<p> A poem is alive</p> <p> Spurring from the inside </p> <p> A poem is a tear </p> <p> From someone's many fears </p> <p> A poem is a smile </p><p> From moments all worthwhile </p> <p> A poem is a light </p> <p> Shining from day to night </p> <p> Poems will be forevermore </p><p> From first light, until we snore </p>"
+	var poemHTML = "<p> A poem is alive</p> <p> Spurring from the inside </p> <p> A poem is a tear </p> <p> From someone's many fears </p> <p> A poem is a smile </p><p> From moments all worthwhile </p> <p> A poem is a light </p> <p> Shining from day to night </p> <p> Poems will be forevermore </p><p> From first light, until we snore </p>";
 	document.getElementById('heading').innerHTML = headerHTML;
 	document.getElementById('contentArea').innerHTML = poemHTML;
 }
 
 function poetryRevised() {
 	var headerHTML = "<h2>	poetry - revised version </h2>";
-	var poemHTML = "<p> A poem is alive</p> <p> Spurring from the inside </p> <p> A poem is a tear </p> <p> From someone's many fears </p> <p> A poem is a smile </p><p> From moments all worthwhile </p> <p> A poem is a light </p> <p> Shining from day to night </p> <p> Poems will be forevermore </p><p> From first light, 'till final war. </p>"
+	var poemHTML = "<p> A poem is alive</p> <p> Spurring from the inside </p> <p> A poem is a tear </p> <p> From someone's many fears </p> <p> A poem is a smile </p><p> From moments all worthwhile </p> <p> A poem is a light </p> <p> Shining from day to night </p> <p> Poems will be forevermore </p><p> From first light, 'till final war. </p>";
 	document.getElementById('heading').innerHTML = headerHTML;
 	document.getElementById('contentArea').innerHTML = poemHTML;
 }
@@ -17,6 +17,44 @@ function poetryAnalysis() {
 
 }
 
+function singaporeOriginal() {
+	var headerHTML = "<h2>	singapore - original version </h2>";
+	var poemHTML = "<p> A poem is alive</p> <p> Spurring from the inside </p> <p> A poem is a tear </p> <p> From someone's many fears </p> <p> A poem is a smile </p><p> From moments all worthwhile </p> <p> A poem is a light </p> <p> Shining from day to night </p> <p> Poems will be forevermore </p><p> From first light, 'till final war. </p>";
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = poemHTML;
+}
+
+function singaporeRevised() {
+	var headerHTML = "<h2>	singapore - revised version </h2>";
+	var poemHTML = "<p> I wandered through each perfect street</p> <p> Near where the Lion's roar rings clear </p> <p>And from the sounds of marching feet</p> <p>Resounds a cry that's full of fear</p><p> </p><p>In every face of every child</p><p>In every parents' worried look</p> <p>In every voice all men reviled</p> <p>The toil of living by the book</p> <p></p> <p>How the young citizen's sad sigh</p> <p>Falls on Parliament's silent halls</p> <p>And the hapless young man's cry</p><p>Runs in tears down Istana walls</p>";
+	var poemHTML2 = "<p> But most, through mall-filled streets I hear </p> <p> How seller's cries grow even louder </p> <p> Blast the old grandmothers' weakened ear </p> <p>And blights with fear the foreign onlooker </p>";
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = poemHTML + poemHTML2;
+}
+
+function oCaptainAnalysis() {
+	var headerHTML = "<h2>	o captain! my captain! - original analysis </h2>";
+	var paragraph1HTML = "<p> The poem “O Captain! My Captain!” by famed poet Walt Whitman is an example of classic, structured poetry. “Captain” is a lyric, with strict conformation to rhythm and rhyme, a set quantity of lines and verses, and themed around either life, death, love or flora. Its classic classification is also justified through its usage of strict structure and literary language, such as apostrophes in words, religious references, and simplified ideals. </p>";
+	var paragraph2HTML = "<p> </p>";
+	var paragraph3HTML = "<p>“O Captain! My Captain!” is based around the conclusion of a vessel and it’s crew’s triumphant voyage of war, and with it, the sudden demise of the captain. The poem starts off with the war-waged boat beginning to draw into port, and the subsequent response of happiness from the bystanders. This period of joy, however, is dampened by the sudden demise of the ship’s captain. The second stanza follows the structure set out by the previous stanza, building upon the celebration of the ship’s return and begins to talk about a crew member’s attempt to “rouse the captain from his sleep”. The last poem fragment, however, diverges from the pattern set out by the previous ones. Instead, it begins and ends with the sad state of the captain, with the surrounding celebrations left in the midst. </p>";
+	var paragraph4HTML = "<p>The language of the poem is quite traditional, with literary language and proper grammar utilized throughout the piece. Words like “weather’d” and “ribbon’d”, old terms not present in the current vernacular, are peppered throughout. The style of poem also requires lines having correct grammar. </p>";
+	var paragraph5HTML = "<p>The conflicting ideas of the poem present a specific theme explored: in every war, everyone loses. Regardless of one entity coming out the victor, all parties incur losses, as seen through the poem.</p>";
+	var paragraph6HTML = "<p> </p>";
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = paragraph1HTML + paragraph2HTML + paragraph3HTML + paragraph4HTML + paragraph5HTML + paragraph6HTML;
+}
+
+function oCaptainRevised() {
+	var headerHTML = "<h2>	o captain! my captain! - original analysis </h2>";
+	var paragraph1HTML = "<p> The poem “O Captain! My Captain!” by famed poet Walt Whitman is an example of classic, structured poetry. “Captain” is a lyric, with strict conformation to rhythm and rhyme, a set quantity of lines and verses, and themed around either life, death, love or flora. Its classic classification is also justified through its usage of strict structure and literary language, such as apostrophes in words, religious references, and simplified ideals. </p>";
+	var paragraph2HTML = "<p>The language of the poem is quite traditional, with literary language and proper grammar utilized throughout the piece. Words like “weather’d” and “ribbon’d”, old terms not present in the current vernacular, are peppered throughout. The style of poem also requires lines having correct grammar.</p>";
+	var paragraph3HTML = "<p>The poem is organized into three stanzas with eight lines each, with varying lengths and rhymes peppering each stanza. Each stanza is comprised of two halves, one half with long lines, and the succeeding half possessing shorter ones. The first half speaks in a positive tone about the conclusion of a voyage. The second half, however, talks grimly of the passing of the vessel’s captain. “O Captain! My Captain!”’s rhyme scheme is AABBCDED, repeating through every stanza. An interesting fact about the poem is that all D lines throughout rhyme, regardless of stanza. </p>";
+	var paragraph4HTML = "<p>“O Captain! My Captain!” is based around the conclusion of a vessel and it’s crew’s triumphant voyage of war, and with it, the sudden demise of the captain. The poem starts off with the war-waged boat beginning to draw into port, and the subsequent response of happiness from the bystanders. This period of joy, however, is dampened by the sudden demise of the ship’s captain. The second stanza follows the structure set out by the previous stanza, building upon the celebration of the ship’s return and begins to talk about a crew member’s attempt to “rouse the captain from his sleep”. The last poem fragment, however, diverges from the pattern set out by the previous ones. Instead, it begins and ends with the sad state of the captain, with the surrounding celebrations left in the midst.   </p>";
+	var paragraph5HTML = "<p>Several poetic devices are utilized in “O Captain, My Captain!” to help convey the theme: from simple alliteration, to a range of personified objects. “Flag is flung” and “safe and sound” help provide setting to the reader, contrast the two events, and add a musical quality to the poem. The same purpose is also served by “ring of the bells” and “bugle trill”; perfect examples of onomatopoeia. These create imagery of the celebration on shore. Emphasis of the dead captain is conveyed through the repetition of the line “fallen cold and dead”. This line helps drive in the fact that no amount of rousing and glory will bring back the dead captain. It also shows the true face of war: not all success is sweet, and often comes at great cost. </p>";
+	var paragraph6HTML = "<p>The conflicting ideas of the poem present a specific theme explored: in every war, everyone loses. Regardless of one entity coming out the victor, all parties incur losses, as seen through the poem.</p>";
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = paragraph1HTML + paragraph2HTML + paragraph3HTML + paragraph4HTML + paragraph5HTML + paragraph6HTML;
+}
 
 // TREES
 
