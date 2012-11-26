08e063755a61bc400e4ffa39e4a576d99575527f
Carlos Arcenas <carlosarcenas@yahoo.com>
2012-11-26 18:50:29 +0700

    Changed originalpoetry, moved metro-fied to main stage.

---
+++ originalpoetry/originalpoetry.js
@@ -0,0 +1,35 @@
+
+function poetryOriginal() {
+	var headerHTML = "<h2>	poetry - original version </h2>";
+	var poemHTML = "<p> A poem is alive</p> <p> Spurring from the inside </p> <p> A poem is a tear </p> <p> From someone's many fears </p> <p> A poem is a smile </p><p> From moments all worthwhile </p> <p> A poem is a light </p> <p> Shining from day to night </p> <p> Poems will be forevermore </p><p> From first light, until we snore </p>"
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = poemHTML;
+}
+
+function poetryRevised() {
+	var headerHTML = "<h2>	poetry - revised version </h2>";
+	var poemHTML = "<p> A poem is alive</p> <p> Spurring from the inside </p> <p> A poem is a tear </p> <p> From someone's many fears </p> <p> A poem is a smile </p><p> From moments all worthwhile </p> <p> A poem is a light </p> <p> Shining from day to night </p> <p> Poems will be forevermore </p><p> From first light, 'till final war. </p>"
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = poemHTML;
+}
+
+function poetryAnalysis() {
+
+}
+
+
+// TREES
+
+function treesAnalysis() {
+	var newHTML = "<h2>	trees - original analysis</h2>";
+	var poemHTML = "";
+	document.getElementById('heading').innerHTML = newHTML;
+	document.getElementById('contentArea').innerHTML = poemHTML;
+}
+
+function treesRevised() {
+	var newHTML = "<h2>	trees - revised analysis</h2>";
+	var poemHTML = "";
+	document.getElementById('heading').innerHTML = newHTML;
+	document.getElementById('contentArea').innerHTML = poemHTML;
+}
