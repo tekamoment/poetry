52ccf8724909184fd4814e9319f97495dcac96e2
Carlos Arcenas <carlosarcenas@yahoo.com>
2012-11-29 23:43:10 +0700

    Minor additions (stanza 3? and some summary)

--- narrativepoetry/narrativepoetry.js
+++ narrativepoetry/narrativepoetry.js
@@ -6,14 +6,14 @@ function lightBrigadePoem() {
 }
 
 function lightBrigadeAnalysis(){
-	var headerHTML = "<h2>para-by-para analysis</h2>";
+	var headerHTML = "<h2>para-by-para summary</h2>";
 	var stanza1 = "<h3>stanza one:</h3>" + "<p> The poem begins with the introduction of the Light Brigade: a company composed of ‘the brave six hundred’, riding into what appears to be a futile fight in the Valley of Death. A soldier, presumably the general of the Brigade, shouts for them to charge further on, deeper into the Valley, to begin a strike at the enemy’s guns. </p>";
-	var stanza2 = "<h3>stanza two:</h3>";
-	var stanza3 = "<h3>stanza three:</h3>";
-	var stanza4 = "<h3>stanza four:</h3>";
-	var stanza5 = "<h3>stanza five:</h3>";
-	var stanza6 = "<h3>stanza six:</h3>";
-	var summary = "<h3>summary</h3>";
+	var stanza2 = "<h3>stanza two:</h3>" + "<p>Another command is raised for the Light Brigade to charge onward. Faced with the fact that it is ‘theirs not to make reply, theirs not to reason why’, they follow the vague order to charge and ride into the battle, fully knowing their inevitable fate and their untimely demise. </p>";
+	var stanza3 = "<h3>stanza three:</h3>" + "<p> The Brigade is now riding bravely in the valley, amidst hot gun and cannon-fire being unleashed all around them. They boldly enter the ‘jaws of Death’, straight into the ‘mouth of Hell’, with shot and shell flying at them.</p>";
+	var stanza4 = "<h3>stanza four:</h3>" + "<p>The Brigade begin to brandish their sabers as they come in contact with the army. They plunge into the army, breaking the line of defense the massive enemy has set up. ‘Cossack and Russian’ fall under the might of the Light Brigade and their swordsmanship, and begin to retreat, and the Light Brigade continue their brave advance.  </p>";
+	var stanza5 = "<h3>stanza five:</h3>" + "<p>Cannons all around them continue to fire upon them ‘with shot and shell’, not relenting for as long as ‘horse and hero fall’. What was left of the brave six hundred came back from Hell, escaped the jaws of Death, and lived to tell the tale. </p>";
+	var stanza6 = "<h3>stanza six:</h3>" + "<p> The glory and honour of the Light Brigade’s bravery and massive charge will be recognized worldwide, and people will wonder and marvel of the greatness of the ‘noble six hundred’. </p>";
+	var summary = "<h3>setting:</h3>";
 	document.getElementById('heading').innerHTML = headerHTML;
 	document.getElementById('contentArea').innerHTML = stanza1 + stanza2 + stanza3 + stanza4 + stanza5 + stanza6 + summary;
 }
