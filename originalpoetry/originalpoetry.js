d20ef0839bf82ed391470acee04ee853eb9a6d5b
Carlos Arcenas <carlosarcenas@yahoo.com>
2012-11-29 23:42:51 +0700

    Incorporate images.

--- originalpoetry/originalpoetry.js
+++ originalpoetry/originalpoetry.js
@@ -1,7 +1,7 @@
 
 function poetryOriginal() {
 	var headerHTML = "<h2>	poetry - original version </h2>";
-	var poemHTML = "<p> A poem is alive<br /> Spurring from the inside </p> <p> A poem is a tear <br /> From someone's many fears </p> <p> A poem is a smile <br /> From moments all worthwhile </p> <p> A poem is a light <br /> Shining from day to night </p> <p> Poems will be forevermore <br /> From first light, until we snore </p>";
+	var poemHTML = "<img src = 'poetry.jpg' width = '80%'/>";
 	document.getElementById('heading').innerHTML = headerHTML;
 	document.getElementById('contentArea').innerHTML = poemHTML;
 }
@@ -17,18 +17,21 @@ function poetryAnalysis() {
 	var headerHTML = "<h2> poetry - analysis </h2>";
 	var paragraph1HTML = "<p><em> The poem “Poetry” by Carlos Arcenas, written in 2012 is an example of free verse poetry.</em> </p>";
 	var paragraph2HTML = "<p>I chose this poem as this poem is a true representation, in my mind, of what a poem truly is. A poem is not merely a bunch of words lazily lying on a piece of paper, but it can be whatever someone wants it to be: a light, a tear, or a smile, in the words of the poem.</p>";
-	var paragraph3HTML = "<p>This poem is a free verse poem, but despite that classification, ‘Poetry has structure. The poem is composed of six stanzas of two lines, each stanza based on a particular object a poem can become, save for the last stanza, which explains the everlasting nature of a piece of poetry.</p>";
+	var paragraph3HTML = "<p>This poem is a free verse poem, but despite that classification, ‘Poetry’ has structure. The poem is composed of six stanzas of two lines, each stanza based on a particular object a poem can become, save for the last stanza, which explains the everlasting nature of a piece of poetry.</p>";
 	var paragraph4HTML = "<p>The poem is about the various forms a poem, normally seen as words set to a unique motion, can be seen as. Through ‘Poetry’, poems are seen as lights, tears, smiles and being alive, expressing the varied and wondrous nature of poetry. The last couplet notes that this art form will last forever, until the end of mankind.</p>";
 	var paragraph5HTML = "<p> </p>";
+	var paragraph6HTML = "<p> </p>";
+	var paragraph7HTML = "<p>The major element of poetry present in ‘Poetry’ is the use of metaphors. In every stanza, save for the final one, a poem is compared to an object in everyday life, to explain its versatility. This notion starts off in the first stanza, where poems are noted as being ‘alive’, coming from the inner feelings of the author. The second, third and fourth stanzas continue the metaphor by comparing it to a ‘tear from fears’, a ‘smile from moments worthwhile’, and a ‘light from day to night’. </p>";
+	var paragraph8HTML = "<p> Before putting it into my portfolio, I changed the last line on advice from Ms. Jill. Instead of ending in “from first light, until we snore”, which sounds like a filler, it was changed to “from first light, ‘till final war”. This change in ending makes the poem sound more sincere and heartfelt, instead of an attempt at humor, as it did before. </p>";
+	var paragraph9HTML = "<p> The poem can still be improved through extending the metaphor a few more stanzas, to drive home the notion of a poem’s versatility and ever-changing forms. And despite the change to the last line, it could still end at a more serious note, as “until final war” was just a continued attempt to continue the rhyme, as was the previous version, “until we snore”.</p>";
 	document.getElementById('heading').innerHTML = headerHTML;
-	document.getElementById('contentArea').innerHTML = paragraph1HTML + paragraph2HTML + paragraph3HTML + paragraph4HTML + paragraph5HTML;
+	document.getElementById('contentArea').innerHTML = paragraph1HTML + paragraph2HTML + paragraph3HTML + paragraph4HTML + paragraph5HTML + paragraph6HTML + paragraph7HTML + paragraph8HTML + paragraph9HTML;
 }
 
 // REMEMBER TO CSSIFY THE MIDDLE ALIGNING AND RIGHT-ALIGNMENT/ITALICIZING
 
 function crankRevised() {
 var headerHTML = "<h2>crank model: generosity</h2>";
-// ADD ENCAPSULATORS (restrict span of columns to around 5/6)
 var divopen = "<div class = \"span6\">"
 var stanza1 = "<p> I’ve always loved these <br />small classes<br />where we do nothing.<br />Real work gets done.</p>";
 var speech1 = "<div class = \"text-right\"><em>Hey. Science. What? </em></div>";
@@ -55,7 +58,7 @@ document.getElementById('contentArea').innerHTML = divopen + stanza1 + speech1 +
 
 function singaporeOriginal() {
 	var headerHTML = "<h2>	singapore - original version </h2>";
-	var poemHTML = "<p> A poem is alive</p> <p> Spurring from the inside </p> <p> A poem is a tear </p> <p> From someone's many fears </p> <p> A poem is a smile </p><p> From moments all worthwhile </p> <p> A poem is a light </p> <p> Shining from day to night </p> <p> Poems will be forevermore </p><p> From first light, 'till final war. </p>";
+	var poemHTML = "<img src = 'singapore.jpg' width = '80%' />";
 	document.getElementById('heading').innerHTML = headerHTML;
 	document.getElementById('contentArea').innerHTML = poemHTML;
 }
@@ -75,8 +78,41 @@ function singaporeAnalysis(){
 	var paragraph3HTML = "<p>This poem is a free verse poem, but despite that classification, ‘Poetry has structure. The poem is composed of six stanzas of two lines, each stanza based on a particular object a poem can become, save for the last stanza, which explains the everlasting nature of a piece of poetry.</p>";
 	var paragraph4HTML = "<p>The poem is about the various forms a poem, normally seen as words set to a unique motion, can be seen as. Through ‘Poetry’, poems are seen as lights, tears, smiles and being alive, expressing the varied and wondrous nature of poetry. The last couplet notes that this art form will last forever, until the end of mankind.</p>";
 	var paragraph5HTML = "<p> </p>";
+	var paragraph6HTML = "";
+	var paragraph7HTML = "<p> Before including this poem in my portfolio, I added several words in the final stanza to emphasize the meaning and to follow valuable feedback from Ms. Jill. ‘Even’ was added before ‘louder’ in the second line, ‘weak’ was extended to ‘weakened’ in the third, and ‘foreign’ was added before ‘onlooker’ in the final line. These changes helped with the mood and kept form with the earlier stanzas.</p>";
+	var paragraph8HTML = "";
 	document.getElementById('heading').innerHTML = headerHTML;
 	document.getElementById('contentArea').innerHTML = paragraph1HTML + paragraph2HTML + paragraph3HTML + paragraph4HTML + paragraph5HTML;
+}
+
+// ON THE MONA LISA 
+
+function monaLisa() {
+	var headerHTML = "<h2> on the mona lisa </h2>";
+	var stanza1 = "<div class = \"grid span10\"><div class = \"row\"><div class = \"span5\"><p> Oh, Mona Lisa, hanging in the Louvre, <br /> With those shifty eyes that look like they move,<br />There are portraits by much better artists,<br />Yet this work’s fan base is still the largest<br />So why is this picture still so famous?<br />Despite the fact the artwork is heinous? </p>";
+	var stanza2 = "<p> People pass by murals with not a peep<br />Yet in front of this little work they weep <br />Is it though this woman is pure beauty,<br />Or is it that her face looks quite snooty?<br />Perfect proportions, or flawless features,<br />Is that why people so wish to meet her?</p>";
+	var stanza3 = "<p> But then there are mysteries all abound,<br />“There’s a hidden meaning!” is all they sound,<br />From little rumors to the famous Code<br />Is that what attracts them to her big abode?<br />Endless investigations fill the air,<br />Giving her exposure without compare.</p>";
+	var stanza4 = "<p> So why does the Mona Lisa draw crowds?<br />What makes children and critics feel wowed?<br />With all said before, no-one quite knows why<br />This little painting is very sublime<br />In conclusion, there’s just one thing to say<br />The Mona Lisa will be here to stay.</p></div>";
+	var imgHTML = "<div class = \"span5\"><img src = 'monalisa.jpg' /></div></div></div>";
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = stanza1 + stanza2 + stanza3 + stanza4 + imgHTML;
+}
+
+// INFINITY 
+
+function infinityPoem(){
+	var headerHTML = "<h2> infinity - nadhira syed azman";
+	var imgHTML = "<img src='infinity.png' />";
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = imgHTML;
+}
+
+function infinityAnalysis(){
+	var headerHTML = "<h2> infinity - analysis";
+	var paragraph1HTML = "<p> <em> The poem ‘Infinity’, penned by Nadhira Syed Azman in 2012, is a free verse poem about a lovestruck girl dreaming about how her relationship with her boyfriend will last.</em> </p>";
+	var paragraph2HTML = "";
+	document.getElementById('heading').innerHTML = headerHTML;
+	document.getElementById('contentArea').innerHTML = paragraph1HTML;
 }
 
 // O CAPTAIN MY CAPTAIN
@@ -96,14 +132,9 @@ function oCaptainPoem() {
 
 function oCaptainAnalysis() {
 	var headerHTML = "<h2>	o captain! my captain! - original analysis </h2>";
-	var paragraph1HTML = "<p> The poem “O Captain! My Captain!” by famed poet Walt Whitman is an example of classic, structured poetry. “Captain” is a lyric, with strict conformation to rhythm and rhyme, a set quantity of lines and verses, and themed around either life, death, love or flora. Its classic classification is also justified through its usage of strict structure and literary language, such as apostrophes in words, religious references, and simplified ideals. </p>";
-	var paragraph2HTML = "<p> </p>";
-	var paragraph3HTML = "<p>“O Captain! My Captain!” is based around the conclusion of a vessel and it’s crew’s triumphant voyage of war, and with it, the sudden demise of the captain. The poem starts off with the war-waged boat beginning to draw into port, and the subsequent response of happiness from the bystanders. This period of joy, however, is dampened by the sudden demise of the ship’s captain. The second stanza follows the structure set out by the previous stanza, building upon the celebration of the ship’s return and begins to talk about a crew member’s attempt to “rouse the captain from his sleep”. The last poem fragment, however, diverges from the pattern set out by the previous ones. Instead, it begins and ends with the sad state of the captain, with the surrounding celebrations left in the midst. </p>";
-	var paragraph4HTML = "<p>The language of the poem is quite traditional, with literary language and proper grammar utilized throughout the piece. Words like “weather’d” and “ribbon’d”, old terms not present in the current vernacular, are peppered throughout. The style of poem also requires lines having correct grammar. </p>";
-	var paragraph5HTML = "<p>The conflicting ideas of the poem present a specific theme explored: in every war, everyone loses. Regardless of one entity coming out the victor, all parties incur losses, as seen through the poem.</p>";
-	var paragraph6HTML = "<p> </p>";
+	var paragraph1HTML = "<img width = '75%' src = 'ocaptainmycaptain.jpg'/>";
 	document.getElementById('heading').innerHTML = headerHTML;
-	document.getElementById('contentArea').innerHTML = paragraph1HTML + paragraph2HTML + paragraph3HTML + paragraph4HTML + paragraph5HTML + paragraph6HTML;
+	document.getElementById('contentArea').innerHTML = paragraph1HTML;
 }
 
 
@@ -131,7 +162,7 @@ function treesPoem() {
 
 function treesAnalysis() {
 	var newHTML = "<h2>	trees - original analysis</h2>";
-	var poemHTML = "";
+	var poemHTML = "<img src = 'trees.jpg' width = '80%' />";
 	document.getElementById('heading').innerHTML = newHTML;
 	document.getElementById('contentArea').innerHTML = poemHTML;
 }
@@ -141,8 +172,8 @@ function treesRevised() {
 	var paragraph1HTML = "<p>The poem “Trees” by poet Joyce Kilmer published in 1919 is an example of classic, structured poetry. “Trees” is a lyric, with strict conformation to rhythm and rhyme, a set quantity of lines and verses and themed around either life, death, love or, in the case of this poem, flora. Its classic classification is also justified through the presence of vintage language, and its publication in the first half of the 20th century.</p>";
 	var paragraph2HTML = "<p>Despite the fact that it is a classic, the language of the poem is only slightly traditional, in contrast to the verbose and flowery language of poems at the time. Someone who reads the poem today can assume that the poem was written recently due to the simplicity of the language used. Lines are constructed as simple sentence fragments, and have similar features, such as language and grammatical structure, as the speech of today.</p>";
 	var paragraph3HTML = "<p>The poem is organized into six stanzas of two lines each, with each line conforming to a certain length determined by the rhythm of iambic tetrameter. “Tree”’s rhyme scheme is AABBCCDDEEAA. This shows that the poem is composed of six rhyming couplets, with the final couplet having the same rhyme as the first stanza. </p>";
-	var paragraph4HTML = "<p>““Tree” is based around the author’s love and admiration for trees, and thanking God for their creation. The poem begins with the author’s acclamation that there “will never be a poem / as lovely as a tree”. Throughout the poem a tree is not referred to as an inanimsate object; rather, the tree is seen as a majestic creation only feasible through the power of God. This is seen through the author’s description of what makes a tree beautiful; its skyward stature, its connection to the bosom of the earth, its summer garb, its winter dress. The author contrasts each pair of ideas to convey the versatility of the tree in all situations: from the treetops pointing at the sky to the roots, hugging the earth, in the cold times of winter and in the heat of summer. The poem concludes with the author’s humble admission that “poems are created by fools like me / but only God can make a tree”. Through the very gracious language utilized by the author, it makes the poem feel like a prayer to God; to give thanks for the wonderful creation that is the tree. </p>";
-	var paragraph5HTML = "<p>ASK JILL FOR HELP WITH THIS PARAGRAPH.</p>";
+	var paragraph4HTML = "<p>““Tree” is based around the author’s love and admiration for trees, and thanking God for their creation. The poem begins with the author’s acclamation that there “will never be a poem / as lovely as a tree”. Throughout the poem a tree is not referred to as an inanimate object; rather, the tree is seen as a majestic creation only feasible through the power of God. This is seen through the author’s description of what makes a tree beautiful; its skyward stature, its connection to the bosom of the earth, its summer garb, its winter dress. The author contrasts each pair of ideas to convey the versatility of the tree in all situations: from the treetops pointing at the sky to the roots, hugging the earth, in the cold times of winter and in the heat of summer. The poem concludes with the author’s humble admission that “poems are created by fools like me / but only God can make a tree”. Through the very gracious language utilized by the author, it makes the poem feel like a prayer to God; to give thanks for the wonderful creation that is the tree. </p>";
+	var paragraph5HTML = "<p>“Tree”‘s theme explores the beauty ; it humbles the lowly man, and praises the power of nature and of God. Several poetic devices are present in “Trees” to help convey the theme: from a light sprinkling of similes to the ever-present personification. Similes are used in the first and final couplets of the poem to provide a base from where the tree praising can begin from. Personification is the main device used in this piece; talking of the tree as a person rather than an average object enhances the author’s message of the tree’s greatness, and lets the reader truly imagine the grace and majesty of the tree. </p>";
 	var paragraph6HTML = "<p>The constant usage of poetic devices and the author’s vivid imagery make apparent the theme of the poem: one of true nature, of awe and of thanks, for the magnificent creation that is the tree.</p>";
 	document.getElementById('heading').innerHTML = headerHTML;
 	document.getElementById('contentArea').innerHTML = paragraph1HTML + paragraph2HTML + paragraph3HTML + paragraph4HTML + paragraph5HTML + paragraph6HTML;
